f5445ac8342434b6f1cfae5071961f4dd38e7812
Priyam Jaiswal <priyamj608@gmail.com>
2026-09-25 23:28:45 +0530

    Time: 16 ms (39.5%), Space: 46.8 MB (99.79%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,52 +1,25 @@
-class Pair {
-    int first;
-    String second;
-
-    Pair(int f, String s) {
-        first = f;
-        second = s;
-    }
-}
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
 
-        HashMap<Integer, Integer> freq = new HashMap<>();
+       HashMap<Integer, Integer> freq = new HashMap<>();
+       for(int i : nums){
+         freq.put(i, freq.getOrDefault(i,0)+1);
+       }
 
-        for (int i = 0; i < nums.length; i++) {                      // Frequency map
-            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
+       PriorityQueue<Integer> pq = new PriorityQueue<>(
+        (a,b) -> freq.get(a) - freq.get(b));
+
+        for(int i : freq.keySet()){
+            pq.offer(i);
+
+            if(pq.size() > k)  pq.poll();
         }
 
-        PriorityQueue<Pair> pq = new PriorityQueue<>(             // Min heap
-            (a, b) -> {
-                if (a.first != b.first)  return a.first - b.first; 
-                
-                return a.second.compareTo(b.second);
-            }
-        );
-
-        for (Map.Entry<Integer, Integer> i : freq.entrySet()) {       // Keep only k most frequent elements
-
-            int element = i.getKey();
-            int frequency = i.getValue();
-
-            Pair curr = new Pair(frequency, String.valueOf(element));
-
-            if (pq.size() < k) {
-                pq.offer(curr);
-                continue;
-            }
-
-            if (curr.first > pq.peek().first) {
-                pq.poll();
-                pq.offer(curr);
-            }
+        int[] res = new int[k];
+        for(int i=0; i<k; i++){
+        res[i] = pq.poll();
         }
 
-        int[] res = new int[k];                       // for  answer
-        for (int i = 0; i < k; i++) {
-            res[i] = Integer.parseInt(pq.poll().second);
-        }
-
-        return res; 
+        return res;
     }
 }
